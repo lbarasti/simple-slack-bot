9c0db9e820d72a237c2061b1d4d0d7f4b86b1658
lorenzo.barasti <lorenzo.barasti@ocado.com>
2016-05-07 18:36:46 +0100

    Disable GIFs

--- janus.rb
+++ janus.rb
@@ -1,5 +1,9 @@
 require 'slack-ruby-bot'
 
+SlackRubyBot.configure do |config|
+  config.send_gifs = false
+end
+
 class PongBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
